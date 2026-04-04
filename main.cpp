6238a0ab9a7f47071045cb913ec702ab36743a15
Lizhije <3426619342@qq.com>
2026-04-04 19:57:38 +0800

    Refactor coroutine tasks and implement sleep functionality with logging

--- main.cpp
+++ main.cpp
@@ -1,10 +1,13 @@
 #include <chrono>
 #include <coroutine>
+#include <deque>
+#include <queue>
 #include <spdlog/sinks/stdout_color_sinks.h>
 #include <spdlog/spdlog.h>
 
-struct RepeatAwaiter // awaiter(原始指针) / awaitable(operator->)
-{
+using namespace std::chrono_literals;
+
+struct RepeatAwaiter {
   bool await_ready() const noexcept { return false; }
 
   std::coroutine_handle<>
@@ -16,11 +19,6 @@ struct RepeatAwaiter // awaiter(原始指针) / awaitable(operator->)
   }
 
   void await_resume() const noexcept {}
-};
-
-struct RepeatAwaitable // awaitable(operator->)
-{
-  RepeatAwaiter operator co_await() { return RepeatAwaiter(); }
 };
 
 struct PreviousAwaiter {
@@ -39,108 +37,201 @@ struct PreviousAwaiter {
   void await_resume() const noexcept {}
 };
 
-struct Promise {
-  auto initial_suspend() { return std::suspend_always(); }
+template <class T> struct Promise {
+  auto initial_suspend() noexcept { return std::suspend_always(); }
 
   auto final_suspend() noexcept { return PreviousAwaiter(mPrevious); }
 
-  void unhandled_exception() { throw; }
+  void unhandled_exception() noexcept { mException = std::current_exception(); }
 
-  auto yield_value(int ret) {
-    mRetValue = ret;
+  auto yield_value(T ret) noexcept {
+    new (&mResult) T(std::move(ret));
     return std::suspend_always();
   }
 
-  void return_void() { mRetValue = 0; }
+  void return_value(T ret) noexcept { new (&mResult) T(std::move(ret)); }
+
+  T result() {
+    if (mException) [[unlikely]] {
+      std::rethrow_exception(mException);
+    }
+    T ret = std::move(mResult);
+    mResult.~T();
+    return ret;
+  }
 
   std::coroutine_handle<Promise> get_return_object() {
     return std::coroutine_handle<Promise>::from_promise(*this);
   }
 
-  int mRetValue;
-  std::coroutine_handle<> mPrevious = nullptr;
+  std::coroutine_handle<> mPrevious{};
+  std::exception_ptr mException{};
+  union {
+    T mResult;
+  };
+
+  Promise() noexcept {}
+  Promise(Promise &&) = delete;
+  ~Promise() {}
 };
 
-struct Task {
-  using promise_type = Promise;
+template <> struct Promise<void> {
+  auto initial_suspend() noexcept { return std::suspend_always(); }
 
-  Task(std::coroutine_handle<promise_type> coroutine) : mCoroutine(coroutine) {}
+  auto final_suspend() noexcept { return PreviousAwaiter(mPrevious); }
+
+  void unhandled_exception() noexcept { mException = std::current_exception(); }
+
+  void return_void() noexcept {}
+
+  void result() {
+    if (mException) [[unlikely]] {
+      std::rethrow_exception(mException);
+    }
+  }
+
+  std::coroutine_handle<Promise> get_return_object() {
+    return std::coroutine_handle<Promise>::from_promise(*this);
+  }
+
+  std::coroutine_handle<> mPrevious{};
+  std::exception_ptr mException{};
+
+  Promise() = default;
+  Promise(Promise &&) = delete;
+  ~Promise() = default;
+};
+
+template <class T = void> struct Task {
+  using promise_type = Promise<T>;
+
+  Task(std::coroutine_handle<promise_type> coroutine) noexcept
+      : mCoroutine(coroutine) {}
 
   Task(Task &&) = delete;
 
   ~Task() { mCoroutine.destroy(); }
 
-  std::coroutine_handle<promise_type> mCoroutine;
-};
-
-struct WorldTask {
-  using promise_type = Promise;
-
-  WorldTask(std::coroutine_handle<promise_type> coroutine)
-      : mCoroutine(coroutine) {}
-
-  WorldTask(WorldTask &&) = delete;
-
-  ~WorldTask() { mCoroutine.destroy(); }
-
-  struct WorldAwaiter {
+  struct Awaiter {
     bool await_ready() const noexcept { return false; }
 
-    std::coroutine_handle<>
+    std::coroutine_handle<promise_type>
     await_suspend(std::coroutine_handle<> coroutine) const noexcept {
       mCoroutine.promise().mPrevious = coroutine;
       return mCoroutine;
     }
 
-    void await_resume() const noexcept {}
+    T await_resume() const { return mCoroutine.promise().result(); }
 
     std::coroutine_handle<promise_type> mCoroutine;
   };
 
-  auto operator co_await() { return WorldAwaiter(mCoroutine); }
+  auto operator co_await() const noexcept { return Awaiter(mCoroutine); }
+
+  operator std::coroutine_handle<>() const noexcept { return mCoroutine; }
 
   std::coroutine_handle<promise_type> mCoroutine;
 };
 
-WorldTask word() {
-  SPDLOG_DEBUG("World");
-  co_yield 422;
-  co_yield 444;
+struct Loop {
+  std::deque<std::coroutine_handle<>> mReadyQueue;
+
+  struct TimerEntry {
+    std::chrono::system_clock::time_point expireTime;
+    std::coroutine_handle<> coroutine;
+
+    bool operator<(TimerEntry const &that) const noexcept {
+      return expireTime > that.expireTime;
+    }
+  };
+
+  std::priority_queue<TimerEntry> mTimerHeap;
+
+  void addTask(std::coroutine_handle<> coroutine) {
+    mReadyQueue.push_front(coroutine);
+  }
+
+  void addTimer(std::chrono::system_clock::time_point expireTime,
+                std::coroutine_handle<> coroutine) {
+    mTimerHeap.push({expireTime, coroutine});
+  }
+
+  void runAll() {
+    while (!mTimerHeap.empty() || !mReadyQueue.empty()) {
+      while (!mReadyQueue.empty()) {
+        auto coroutine = mReadyQueue.front();
+        mReadyQueue.pop_front();
+        coroutine.resume();
+      }
+      if (!mTimerHeap.empty()) {
+        auto nowTime = std::chrono::system_clock::now();
+        auto timer = std::move(mTimerHeap.top());
+        if (timer.expireTime < nowTime) {
+          mTimerHeap.pop();
+          timer.coroutine.resume();
+        } else {
+          std::this_thread::sleep_until(timer.expireTime);
+        }
+      }
+    }
+  }
+
+  Loop &operator=(Loop &&) = delete;
+};
+
+Loop &getLoop() {
+  static Loop loop;
+  return loop;
+}
+
+struct SleepAwaiter {
+  bool await_ready() const noexcept { return false; }
+
+  void await_suspend(std::coroutine_handle<> coroutine) const {
+    getLoop().addTimer(mExpireTime, coroutine);
+  }
+
+  void await_resume() const noexcept {}
+
+  std::chrono::system_clock::time_point mExpireTime;
+};
+
+Task<void> sleep_until(std::chrono::system_clock::time_point expireTime) {
+  co_await SleepAwaiter(expireTime);
   co_return;
 }
 
-Task hello() {
-  SPDLOG_DEBUG("Hello 正在构建");
-  WorldTask worldTask = word();
-  SPDLOG_DEBUG("Hello 构建完成");
-  co_await worldTask;
-  SPDLOG_DEBUG("hello 得到word的结果: {}",
-               worldTask.mCoroutine.promise().mRetValue);
-  co_await worldTask;
-  SPDLOG_DEBUG("hello 再次得到word的结果: {}",
-               worldTask.mCoroutine.promise().mRetValue);
-  SPDLOG_DEBUG("hello 42");
-  co_yield 42;
-  SPDLOG_DEBUG("hello 12");
-  co_yield 12;
-  SPDLOG_DEBUG("hello 6");
-  co_yield 6;
-  SPDLOG_DEBUG("hello 结束");
+Task<void> sleep_for(std::chrono::system_clock::duration duration) {
+  co_await SleepAwaiter(std::chrono::system_clock::now() + duration);
   co_return;
+}
+
+Task<int> hello1() {
+  SPDLOG_DEBUG("hello1开始睡1秒");
+  co_await sleep_for(1s); // 1s 等价于 std::chrono::seconds(1)
+  SPDLOG_DEBUG("hello1睡醒了");
+  co_return 1;
+}
+
+Task<int> hello2() {
+  SPDLOG_DEBUG("hello2开始睡2秒");
+  co_await sleep_for(2s); // 2s 等价于 std::chrono::seconds(2)
+  SPDLOG_DEBUG("hello2睡醒了");
+  co_return 2;
 }
 
 int main(int argc, char *argv[]) {
-  auto logger = spdlog::stdout_color_mt("console");
+  const auto logger = spdlog::stdout_color_mt("console");
   spdlog::set_default_logger(logger);
   spdlog::set_pattern("[%H:%M:%S] [%^%l%$] %v");
   spdlog::set_level(spdlog::level::debug);
 
-  SPDLOG_DEBUG("Starting main");
-  auto task = hello();
-  while (!task.mCoroutine.done()) {
-    task.mCoroutine.resume();
-    SPDLOG_DEBUG("main得到hello的结果为{}",
-                 task.mCoroutine.promise().mRetValue);
-  }
+  auto t1 = hello1();
+  auto t2 = hello2();
+  getLoop().addTask(t1);
+  getLoop().addTask(t2);
+  getLoop().runAll();
+  SPDLOG_DEBUG("主函数中得到hello1结果: {}", t1.mCoroutine.promise().result());
+  SPDLOG_DEBUG("主函数中得到hello2结果: {}", t2.mCoroutine.promise().result());
   return 0;
 }
